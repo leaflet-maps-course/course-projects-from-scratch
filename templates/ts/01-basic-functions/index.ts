a6b8afc552adad332599d73e17c2e231b2257f72
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-21 19:06:12 +0200

    4.- ESLint Configurado y ejecutado

--- templates/ts/01-basic-functions/index.ts
+++ templates/ts/01-basic-functions/index.ts
@@ -1,6 +1,6 @@
-import { Map, tileLayer} from 'leaflet'
+import { Map, tileLayer} from 'leaflet';
 
-const mymap = new Map("map").setView([43.3082977,-1.9837398], 10);
+const mymap = new Map('map').setView([43.3082977,-1.9837398], 10);
 
 tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
