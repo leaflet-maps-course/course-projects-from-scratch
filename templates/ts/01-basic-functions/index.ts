ac70e0e5419c348a27ae9f36ff11ef022c43ebc7
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-21 18:40:08 +0200

    03-Primer mapa con TS y plantilla creada

---
+++ templates/ts/01-basic-functions/index.ts
@@ -0,0 +1,8 @@
+import { Map, tileLayer} from 'leaflet'
+
+const mymap = new Map("map").setView([43.3082977,-1.9837398], 10);
+
+tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+}).addTo(mymap);
