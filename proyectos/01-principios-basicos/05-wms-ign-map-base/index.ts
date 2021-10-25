40d63caecb59c795d2e05a1c30428f4e93dc7936
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-25 18:15:51 +0200

    11.- Práctica - Refactorizar con nueva función proyecto con WMS

--- proyectos/01-principios-basicos/05-wms-ign-map-base/index.ts
+++ proyectos/01-principios-basicos/05-wms-ign-map-base/index.ts
@@ -1,11 +1,15 @@
-import { Map, tileLayer} from 'leaflet';
+import { Map} from 'leaflet';
 import { tileLayers } from '../../../config/tile-layers/data';
+import { tileLayerWMSSelect } from '../../../config/tile-layers/functions';
 
 const mymap = new Map('map').setView([40.4378698,-3.8196223], 11);
 
-tileLayer.wms('https://www.ign.es/wms-inspire/ign-base?', {
-    layers: 'IGNBaseTodo',
-    format: 'image/png',
-    transparent: true,
-    attribution: tileLayers.baseLayers.default.atribution
-}).addTo(mymap);
+tileLayerWMSSelect(
+    'https://www.ign.es/wms-inspire/ign-base?',
+    {
+        layers: 'IGNBaseTodo',
+        format: 'image/png',
+        transparent: true,
+        attribution: tileLayers.baseLayers.default.atribution
+    }
+).addTo(mymap);
