1194196695e3355696f3cb9427f2f92be145ea83
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-25 17:28:00 +0200

    Proyecto 04 - WMS código implementado

--- proyectos/01-principios-basicos/04-basic-wms-mundialis/index.ts
+++ proyectos/01-principios-basicos/04-basic-wms-mundialis/index.ts
@@ -1,8 +1,11 @@
 import { Map, tileLayer} from 'leaflet';
+import { tileLayers } from '../../../config/tile-layers/data';
 
 const mymap = new Map('map').setView([43.3082977,-1.9837398], 10);
 
-tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+tileLayer.wms('http://ows.mundialis.de/services/service?', {
+	layers: 'Dark',
+	format: 'image/png',
+	transparent: true,
+	attribution: tileLayers.baseLayers.default.atribution
 }).addTo(mymap);
