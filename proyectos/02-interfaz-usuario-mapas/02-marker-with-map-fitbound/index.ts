9089c32c01b84dbe1091c46881f041dc9342f186
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-26 19:09:54 +0200

    2.- map.fitBounds() - centrando en base a seleccción

---
+++ proyectos/02-interfaz-usuario-mapas/02-marker-with-map-fitbound/index.ts
@@ -0,0 +1,29 @@
+import { tileLayers } from '../../../config/tile-layers/data';
+import { Map, marker } from 'leaflet';
+import { startMapTemplate } from '../../../assets/template/content';
+import { tileLayerSelect } from '../../../config/tile-layers/functions';
+
+startMapTemplate(document, 'Sección 2 - 2 - Centrando Mapa con fitBounds');
+
+// 1 .- Asignamos ubicación (0, 0)
+const mymap = new Map('map').setView([0, 0], 11); //
+
+// 2.- Especificamos el tipo de mapa
+tileLayerSelect(tileLayers.baseLayers.thunderForest.map.atlas, {
+    attribution: tileLayers.baseLayers.thunderForest.atribution
+}).addTo(mymap);
+
+// 3.- Marcadores 
+
+// Torre Eiffel
+const eiffelPointMarker = marker([48.8581873,2.2933053]).addTo(mymap);
+// Notre Dame
+const notreDamePointMarker = marker([48.8528799,2.3497519]).addTo(mymap);
+
+// 4.- Centrar con los elementos especificados como los dos marcadores
+
+mymap.fitBounds([
+    [eiffelPointMarker.getLatLng().lat, eiffelPointMarker.getLatLng().lng],
+    // Sid ejamos esto, se centra en base a este
+    [notreDamePointMarker.getLatLng().lat, notreDamePointMarker.getLatLng().lng]
+])
