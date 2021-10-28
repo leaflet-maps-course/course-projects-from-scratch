db6438fd6f365126680780b3ca5d7c313b10fe5e
Anartz Mugika Ledo <mugan86@gmail.com>
2021-10-28 20:43:49 +0200

    Project0 primera parte marcadores finalizado

---
+++ assets/data/interest-points/api-data.ts
@@ -0,0 +1,3 @@
+export const INTEREST_POINTS = {
+    DONOSTIA_SANSEBASTIAN : "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/practice-projects-material/section-2-ui-maps/01-markers-popup-tooltips/data/interest_points.json"
+};
---
+++ proyectos/02-interfaz-usuario-mapas/10-project-markers-popup-tooltip/index.ts
@@ -0,0 +1,43 @@
+import { tileLayers } from '../../../config/tile-layers/data';
+import { Map, marker } from 'leaflet';
+import { startMapTemplate } from '../../../assets/template/content';
+import { tileLayerSelect } from '../../../config/tile-layers/functions';
+import axios from 'axios';
+import { INTEREST_POINTS } from '../../../assets/data/interest-points/api-data';
+
+startMapTemplate(document, 'Sección 2 - 10 - Repasando con Marcadores, Popup y Tooltip');
+
+// 1 .- Asignamos ubicación (Donostia - San Sebastian) (Cogemos desde API)
+const mymap = new Map('map').setView([43.32, -1.98], 11);
+
+// 2.- Especificamos el tipo de mapa
+tileLayerSelect(tileLayers.baseLayers.osmHot.map, {
+    attribution: tileLayers.baseLayers.osmHot.atribution
+}).addTo(mymap);
+
+// 3.- Llamamos a la fuente de datos
+axios.get(INTEREST_POINTS.DONOSTIA_SANSEBASTIAN)
+.then((result: {data: { 
+    title: string, description: string,
+    location: {lat: number, lng: number},
+    points: Array<{
+        lat: number, lng: number, info: string, name: string, img: string
+    }>
+}}) => {
+    // 4.- Cargamos los marcadores y añadimos en el  mapa
+    const points = result.data.points;
+    result.data.points.map((pointElement) => {
+        marker([pointElement.lat, pointElement.lng]).addTo(mymap).bindPopup(`
+            <h4> ${pointElement.name} </h4>
+            <p> Ubicación: ${pointElement.lat},${pointElement.lng}</p>
+            <img src="${pointElement.img}" width="200px"/><br/>
+            ${(pointElement.info !== "") ? `<a class="info" href="${pointElement.info}" target="_blank"> Más información</a>`: ``}            
+        `);
+    });
+    
+    // 5.- Centramos en base a la selección
+    mymap.fitBounds([
+        ...points.map((point) => [point.lat, point.lng] as [number, number])
+    ]);
+})
+
