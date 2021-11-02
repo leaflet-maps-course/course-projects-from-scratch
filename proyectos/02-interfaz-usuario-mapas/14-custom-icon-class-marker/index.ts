d189c57c792465c1e0608617f52969e97f51a41b
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-02 20:50:07 +0100

    Sección 2 - 14 - Marcadores personalizados - Crear nuestro Clase Icon para reutilizar propiedades

---
+++ proyectos/02-interfaz-usuario-mapas/14-custom-icon-class-marker/index.ts
@@ -0,0 +1,77 @@
+import { tileLayers } from "../../../config/tile-layers/data";
+import { Icon, IconOptions, Map, marker } from "leaflet";
+import { startMapTemplate } from "../../../assets/template/content";
+import { tileLayerSelect } from "../../../config/tile-layers/functions";
+
+startMapTemplate(
+  document,
+  "Sección 2 - 14 - Marcadores con iconos personalizados usando clase de icono personalizado"
+);
+// https://leafletjs.com/reference-1.7.1.html#icon
+// https://github.com/pointhi/leaflet-color-markers/
+// 1 .- Asignamos ubicación (Nueva York)
+const mymap = new Map("map").setView([40.712728, -74.006015], 11);
+
+// 2.- Especificamos el tipo de mapa
+tileLayerSelect(tileLayers.baseLayers.stadia.map.Outdoors, {
+  attribution: tileLayers.baseLayers.stadia.atribution,
+}).addTo(mymap);
+
+
+// 3.- Icono de clase personalizado donde vamos a compartir las propiedades excepto 
+// URL del icono
+
+const CustomIcon = (options: IconOptions) => Icon.extend({
+  options: {
+      iconUrl: options.iconUrl,
+      shadowUrl: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+      iconSize: [38, 95],
+      shadowSize: [50, 64],
+      iconAnchor: [22, 94],
+      shadowAnchor: [4, 62],
+      popupAnchor: [-3, -76],
+  }
+});
+
+
+// 4.- Marcadores personalizados con su icono
+const leafGreenIcon = CustomIcon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png"
+});
+
+const leafRedIcon = CustomIcon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-red.png"
+});
+
+// Hollywood
+const hollywoodPoint = marker([34.098333333333, -118.32666666667], {
+  icon: new leafGreenIcon,
+})
+  .bindPopup(
+    `
+<h4> Hollywood </h4>
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Wiki_training_0226.jpg/800px-Wiki_training_0226.jpg" />
+`
+  )
+  .addTo(mymap);
+
+// Aeropuerto Internacional LA
+const airportInternationalLAPoint = marker([33.9425, -118.408056], {
+  icon: new leafRedIcon,
+})
+  .addTo(mymap)
+  .bindPopup(
+    `
+    <h5>Aeropuerto Internacional de Los Angeles</h5>
+    <p><a href="https://es.wikipedia.org/wiki/Aeropuerto_Internacional_de_Los_%C3%81ngeles" target="_blank"> Más información</a></p>
+`
+  )
+  .openPopup();
+
+mymap.fitBounds([
+  [hollywoodPoint.getLatLng().lat, hollywoodPoint.getLatLng().lng],
+  [
+    airportInternationalLAPoint.getLatLng().lat,
+    airportInternationalLAPoint.getLatLng().lng,
+  ],
+]);
