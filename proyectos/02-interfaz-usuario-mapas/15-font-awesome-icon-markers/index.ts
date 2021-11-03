4975865a865b0ed38977ee3516b32c73e32be71b
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-03 20:39:46 +0100

    Sección 2 - 15 - Font Awesome con DivIcon

--- proyectos/02-interfaz-usuario-mapas/15-font-awesome-icon-markers/index.ts
+++ proyectos/02-interfaz-usuario-mapas/15-font-awesome-icon-markers/index.ts
@@ -1,9 +1,9 @@
 import { tileLayers } from '../../../config/tile-layers/data';
-import { divIcon, Map, marker } from 'leaflet';
+import { divIcon, Map, marker, Point } from 'leaflet';
 import { startMapTemplate } from '../../../assets/template/content';
 import { tileLayerSelect } from '../../../config/tile-layers/functions';
 
-startMapTemplate(document, 'Sección 2 - 1 - Primer marcador en Leaflet');
+startMapTemplate(document, 'Sección 2 - 15 - Iconos con Capas personalizadas + Fuentes - DivICon + FontAwesome ');
 
 // 1 .- Asignamos ubicación (Paris)
 const mymap = new Map('map').setView([48.8588336,2.276995], 11);
@@ -14,20 +14,12 @@ tileLayerSelect(tileLayers.baseLayers.osmHot.map, {
 }).addTo(mymap);
 
 // 3.- Iconos de capa (DivIcon)
-
-// coffe
-
-const coffeCustomDivIcon = divIcon({
-    className: "custom-div-icon",
-    html: "<div class='marker-pin green'></div><i class=\"fas fa-mug-hot\"></i>",
-    iconSize: [30, 42],
-    iconAnchor: [15, 42],
-  });
-
+const myIcon = divIcon({className: 'my-div-icon', html: '<i class="fas fa-ethernet"></i>'});
+// you can set .my-div-icon styles in CSS
 
   // Torre Eiffel
-marker([48.8581873,2.2933053], {
-    icon: coffeCustomDivIcon,
-  }).addTo(mymap);
+marker([48.8581873,2.2933053], {icon: myIcon}).addTo(mymap).bindPopup("Torre Eiffel", {
+  offset: [6, 2] // Para ubicar el cursor del popup
+});
 // Notre Dame
 marker([48.8528799,2.3497519]).addTo(mymap);
