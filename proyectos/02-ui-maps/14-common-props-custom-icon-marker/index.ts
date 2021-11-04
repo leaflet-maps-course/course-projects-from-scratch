1325f913cb7459f88e6ef2a85201dab85fb5a653
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-04 18:56:13 +0100

    Sección 2 - 14 - Reutilizando configuraciones de los iconos

---
+++ proyectos/02-ui-maps/14-common-props-custom-icon-marker/index.ts
@@ -0,0 +1,70 @@
+import { Icon, IconOptions, Map, marker } from "leaflet";
+import { drinkWaterSoraluze } from "../../../assets/data/markers/drink_waters";
+import { startMapTemplate } from "../../../assets/template/content";
+import { tileLayers } from "../../../config/tile-layers/data";
+import { tileLayerSelect } from "../../../config/tile-layers/functions";
+
+startMapTemplate(
+  document,
+  "Sección 2 - 14 - Reutilizando propiedades de marcadores en diferentes colores"
+);
+
+const mymap = new Map("map").setView([0, 0], 12);
+// Maracdores por defecto en otros colores
+// https://github.com/pointhi/leaflet-color-markers/
+tileLayerSelect(tileLayers.baseLayers.thunderForest.map.atlas, {
+  attribution: tileLayers.baseLayers.thunderForest.atribution,
+}).addTo(mymap);
+
+// Carga dinámica de los marcadores
+
+const drinkWaterIcon = new Icon({
+  iconUrl:
+    "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/markers/icons/custom/drink_water.png",
+  shadowUrl:
+    "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/markers/icons/custom/drink_water_shadow.png",
+  iconSize: [41, 41],
+  iconAnchor: [20, 41],
+  popupAnchor: [1, -38],
+  shadowSize: [41, 41],
+});
+
+const CustomDrinkWaterIcon = (options: IconOptions) =>
+  Icon.extend({
+    options: {
+      iconUrl: options.iconUrl
+        ? options.iconUrl
+        : "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/markers/icons/custom/drink_water.png",
+      shadowUrl:
+        "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/markers/icons/custom/drink_water_shadow.png",
+      iconSize: [41, 41],
+      iconAnchor: [20, 41],
+      popupAnchor: [1, -38],
+      shadowSize: [41, 41],
+    },
+  });
+
+const redIcon = CustomDrinkWaterIcon({
+    iconUrl: "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/markers/icons/custom/drink_water_red.png"
+})
+
+const greenIcon = CustomDrinkWaterIcon({
+    iconUrl: "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/markers/icons/custom/drink_water_green.png"
+})
+
+drinkWaterSoraluze.map((point) => {
+  marker([point.lat, point.lon], { icon: new greenIcon() })
+    .addTo(mymap)
+    .bindPopup(point.name);
+});
+
+const drinkWaterEibar = marker([43.1837883, -2.4719938], {
+  icon: new greenIcon(),
+}).addTo(mymap);
+
+mymap.fitBounds([
+  ...drinkWaterSoraluze.map(
+    (point) => [point.lat, point.lon] as [number, number]
+  ),
+  [drinkWaterEibar.getLatLng().lat, drinkWaterEibar.getLatLng().lng],
+]);
