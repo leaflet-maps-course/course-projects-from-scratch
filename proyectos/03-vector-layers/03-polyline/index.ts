7954e62aea61985d0ffcc46927e15fa013bf976b
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-07 19:14:17 +0100

    Sección 3 - 03 - Polyline

--- proyectos/03-vector-layers/03-polyline/index.ts
+++ proyectos/03-vector-layers/03-polyline/index.ts
@@ -22,19 +22,45 @@ tileLayerSelect(tileLayers.baseLayers.hikeBike.map, {
  * desde "Path"
  */
 // Dibujando líneas con Polyline. Se necesita un array con por lo menos dos localizaciones
-// mínimo para formar Un elemento de línea con su inicio y final
+// mínimo para formar Un elemento de línea
 const lineOneItem: [number, number][] = [
   [43.175663, -2.412301],
   [43.172156, -2.413997], // MINIMO HASTA AQUI
 ];
 
-// Creamos los polylines a partir de las coordenadas especificadas
-polyline(lineOneItem, { color: "red", weight: 5 }).addTo(
-  mymap
-).bindTooltip('Línea donde se separan los dos pueblos');
-// Probar con popup, para ver el problema que da con el anclaje
+const lineTwoItem: [number, number][] = [
+  [43.181063, -2.420891],
+  [43.174981, -2.403418], // MINIMO HASTA AQUI
+];
 
+const lineThreeItem: [number, number][] = [
+  [43.166945, -2.39552],
+  [43.166953, -2.403341], // MINIMO HASTA AQUI
+  [43.171572, -2.409777],
+];
+
+// Creamos los polylines a partir de las coordenadas especificadas
+const polylineItem = polyline(lineOneItem, { color: "red", weight: 5 }).addTo(
+  mymap
+);
+const polylineTwoItem = polyline(lineTwoItem, {
+  color: "blue",
+  weight: 10,
+}).addTo(mymap);
+const polylineThreeItem = polyline(lineThreeItem, {
+  color: "green",
+  weight: 3,
+}).addTo(mymap);
 
 // Hacemos zoom teniendo en cuenta los puntos del polyline, nos fijamos en los límites
 // para no tener que añadir todos a la hora de centrar
-mymap.fitBounds(lineOneItem);
+mymap.fitBounds([
+  [
+    polylineTwoItem.getBounds().getNorthWest().lat,
+    polylineTwoItem.getBounds().getNorthWest().lng,
+  ],
+  [
+    polylineThreeItem.getBounds().getSouthEast().lat,
+    polylineThreeItem.getBounds().getSouthEast().lng,
+  ],
+]);
