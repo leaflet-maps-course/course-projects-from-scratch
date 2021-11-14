92bf7311a196c923aedb2bb15466bfcec554321f
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-14 19:30:54 +0100

    Sección 3 - 11 - Read ficheros GPX y obtener posiciones

---
+++ proyectos/03-vector-layers/11-read-gpx-data-and-log/index.ts
@@ -0,0 +1,15 @@
+import gpxParser from 'gpxparser';
+import { startMapTemplate } from '../../../assets/template/content';
+import axios from 'axios';
+
+startMapTemplate(document, 'Sección 3 - 11 - Read GPX Data and Log');
+
+axios.get('https://raw.githubusercontent.com/leaflet-maps-course/resources/main/tracks/track.gpx')
+.then( result => {
+    var gpx = new gpxParser(); //Create gpxParser Object
+    gpx.parse(result.data); //parse gpx file from string data
+    gpx.tracks[0].points.map((point) => console.log(point.lat, point.lon));
+})
+.catch( error => console.error(error));
+
+
