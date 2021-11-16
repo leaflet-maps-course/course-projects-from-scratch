1e50bee99f438ca3e4c50edecc98030cfdd05197
Anartz Mugika Ledo <mugan86@gmail.com>
2021-11-16 20:30:38 +0100

    Sección 4 - 4 - Controles custom - Watermark + locations

---
+++ proyectos/04-controls/04-custom-ctrl-water-mark/index.ts
@@ -0,0 +1,19 @@
+import { PLACES_LIST_LOCATIONS } from "./../../../config/map/locations";
+import { Map } from "leaflet";
+import { startMapTemplate } from "../../../assets/template/content";
+import { tileLayerSelect } from "../../../config/tile-layers/functions";
+import { watermark } from "../custom-controls/watermark";
+
+startMapTemplate(
+  document,
+  "Sección 04 - 04 - Controles - Custom - Marca de agua"
+);
+
+const mymap = new Map("map").setView(
+  PLACES_LIST_LOCATIONS.HONOLULU_HAWAI_EEUU as [number, number],
+  10
+);
+
+tileLayerSelect().addTo(mymap);
+
+watermark({position: 'bottomleft'}).addTo(mymap);
---
+++ proyectos/04-controls/custom-controls/watermark.ts
@@ -0,0 +1,27 @@
+import { Control, ControlPosition, DomUtil, Util } from "leaflet";
+
+const Watermark = Control.extend({
+  // Para llamar al padre
+  initialize: function (options: {position: ControlPosition, img: string, border: true}) {
+    Util.setOptions(this, options); // Sobrescribimos las opciones
+  },
+  // Opciones del control
+  options: {
+    position: this , // Por defecto, añadimos arriba a la izquierda
+    img: "https://raw.githubusercontent.com/leaflet-maps-course/resources/main/logotypes/am.png",
+    border: true,
+  },
+  // Para formar el elemento del control
+  onAdd: function () {
+    // Selector
+    const img = DomUtil.create("img"); // Para crear un elemento de imagen
+    img.src = this.options.img;
+    if (this.options.border) {
+      img.style.borderBottom = "3px solid #506bff";
+    }
+    img.style.width = "40px";
+    return img;
+  },
+});
+
+export const watermark = (options?: {position?: ControlPosition, img?: string, border?: true}) => new Watermark(options);
